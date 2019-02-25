ff62836ca238deba77a108b7b4797fc38c9fc00d
jestin skaria <jestinskaria15@gmail.com>
2019-02-25 14:37:18 -0500

    commit using whileloop

--- C0745078-WEEK07/js/main.js
+++ C0745078-WEEK07/js/main.js
@@ -1,1 +1,15 @@
 ﻿// Your code here!
+function addNumbers() {
+    var sum = 0;
+ /*   for (var i = 117; i >=11;) {
+        sum = sum + i;
+        i = i - 8
+    }
+    */
+    var i = 117;
+    while (i >= 11) {
+        sum = sum + i;
+        i = i - 8
+    }
+    return sum;
+}
