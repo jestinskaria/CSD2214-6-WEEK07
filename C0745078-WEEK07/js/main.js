5163b2d40a2b8b6e1bfc9c82bc1916aa618a31c5
jestin skaria <jestinskaria15@gmail.com>
2019-02-25 15:30:18 -0500

    3.30 pm commit

--- C0745078-WEEK07/js/main.js
+++ C0745078-WEEK07/js/main.js
@@ -13,3 +13,23 @@ function addNumbers() {
     }
     return sum;
 }
+var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOffSpring: 5,
+    noisy: true,
+    deadly: false
+};
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffSpring: 3,
+    noisy: true,
+    deadly: true
+};
